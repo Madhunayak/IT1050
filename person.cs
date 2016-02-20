d4374fe90b34ee781ae670372fc441a9879ec6fc
Madhunayak <Madhusmita_nayak@hotmail.com>
2016-02-19 23:47:43 -0500

    Added files via upload

---
+++ person.cs
@@ -0,0 +1,46 @@
+﻿namespace assignment_2
+{
+    class person
+    {
+
+
+        public string firstname, lastname;
+            public int age;
+        public person spouse;
+      public static double sumofallages;
+        public static int p = 0;
+        public string spousename;
+        
+        
+        
+
+            public string getfullname()
+        {
+            //System.Console.WriteLine("Enter ur first name  ");
+           // this.firstname = System.Console.ReadLine();
+           // System.Console.WriteLine("Enter ur lastname ");
+           // this.lastname = System.Console.ReadLine();
+           
+           // System.Console.WriteLine("Enter ur age ");
+            //this.age =int.Parse( System.Console.ReadLine());
+            return this.firstname + " " + this.lastname;
+        }
+
+
+        public void printNameandAge()
+        {
+            p++;
+            System.Console.WriteLine("person" + p + "'s " + "name is : " + this.getfullname());
+            System.Console.WriteLine("person" + p + "'s " + " age is : " + this.age);
+            System.Console.WriteLine("person" + p + "'s " + "spouse name is : " +this.spousename);
+            System.Console.WriteLine("person" + p + "'s " + "spouse age is : " + this.spouse.age);
+
+        }
+
+    }  
+        
+            
+        }
+        
+    
+
