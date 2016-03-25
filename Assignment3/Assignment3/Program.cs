afdb6ed9a1c7ceed16001a63513cf9a511d84aa1
Madhunayak <Madhusmita_nayak@hotmail.com>
2016-03-24 22:36:59 -0400

    Assignment 3
    madhu

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Instructor John = new Instructor("John", "English");
+            Instructor Mike = new Instructor("MIKE", "Math");
+
+            Student Jane = new Student("Jane", John);
+            Student Joe = new Student("Joe", John);
+
+            Student Melissa = new Student("Melissa", Mike);
+            Student Matt = new Student("Matt", Mike);
+
+            John.SetStudentGrade(Jane, 95);
+            John.SetStudentGrade(Joe, 85);
+            Mike.SetStudentGrade(Melissa, 90);
+            Mike.SetStudentGrade(Matt, 92);
+
+            Jane.PrintStudentInfo();
+            Joe.PrintStudentInfo();
+            Melissa.PrintStudentInfo();
+            Matt.PrintStudentInfo();
+
+            System.Console.WriteLine();
+            System.Console.ReadKey();
+
+
+        }
+        }
+    }
---
+++ Assignment3/Assignment3/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Student
+    {
+        private String Name;
+        private int Grade=0;
+        private Instructor Teacher;
+
+
+        public Student(string Studentname , Instructor Teacher1)
+        {
+            this.Name = Studentname;
+            this.Teacher = Teacher1;
+        }
+
+        public void Setgrade(int SGrade)
+        {
+            this.Grade = SGrade;
+        }
+
+        public void PrintStudentInfo()
+        {
+            System.Console.WriteLine("Student Name: " + this.Name + " Grade: " + this.Grade + Teacher.PrintInstructorInfo());
+            
+        }
+
+    }
+}
