f120934ffbd7342460e2def393414d0926c064ef
Madhunayak <Madhusmita_nayak@hotmail.com>
2016-03-28 01:05:54 -0400

    Added files via upload

---
+++ Instructor1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_3
+{
+    class Instructor1
+    {
+      
+        
+            private String Name;
+            private String CourseName;
+
+
+            public Instructor1(string TName, string TcourseName)
+            {
+                this.Name = TName;
+                this.CourseName = TcourseName;
+
+            }
+            public void SetStudentGrade(Student1 Student2, int Grade1)
+            {
+                Student2.Setgrade(Grade1);
+            }
+
+            public string PrintInstructorInfo()
+            {
+                return (" Instructor Name: -" + this.Name + " Course Name:- " + this.CourseName);
+            }
+        }
+    }
+
+
--- Program.cs
+++ Program.cs
@@ -1,32 +1,36 @@
-﻿namespace Assignment3
+﻿
+namespace Test_3
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Instructor John = new Instructor("John", "English");
-            Instructor Mike = new Instructor("MIKE", "Math");
+      
+            static void Main(string[] args)
+            {
+                Instructor1 John = new Instructor1("John", "English");
+                Instructor1 Mike = new Instructor1("MIKE", "Math");
 
-            Student Jane = new Student("Jane", John);
-            Student Joe = new Student("Joe", John);
+                Student1 Jane = new Student1("Jane", John);
+                Student1 Joe = new Student1("Joe", John);
 
-            Student Melissa = new Student("Melissa", Mike);
-            Student Matt = new Student("Matt", Mike);
+                Student1 Melissa = new Student1("Melissa", Mike);
+                Student1 Matt = new Student1("Matt", Mike);
 
-            John.SetStudentGrade(Jane, 95);
-            John.SetStudentGrade(Joe, 85);
-            Mike.SetStudentGrade(Melissa, 90);
-            Mike.SetStudentGrade(Matt, 92);
+                John.SetStudentGrade(Jane, 95);
+                John.SetStudentGrade(Joe, 85);
+                Mike.SetStudentGrade(Melissa, 90);
+                Mike.SetStudentGrade(Matt, 92);
 
-            Jane.PrintStudentInfo();
-            Joe.PrintStudentInfo();
-            Melissa.PrintStudentInfo();
-            Matt.PrintStudentInfo();
+                Jane.PrintStudentInfo();
+                Joe.PrintStudentInfo();
+                Melissa.PrintStudentInfo();
+                Matt.PrintStudentInfo();
 
-            System.Console.WriteLine();
-            System.Console.ReadKey();
+                System.Console.WriteLine();
+                System.Console.ReadKey();
 
 
-        }
+            }
         }
     }
+    
+
