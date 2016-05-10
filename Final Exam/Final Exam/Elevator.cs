961175dfde5963b2f21a1fe5514c7ab0664a4809
Madhunayak <Madhusmita_nayak@hotmail.com>
2016-05-10 19:21:58 -0400

    madhu

---
+++ Final Exam/Final Exam/Elevator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Exam
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupant;
+
+     
+        public Elevator (int maxOccupant,double  maxWeight)
+        {
+            Occupant = new Passenger[maxOccupant];
+
+            this.MaxWeight = maxWeight;
+          
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+
+            Occupant[index] = passenger;
+        }
+        public double GetCurrentWeight()
+        {
+            double wt = 0;
+            foreach (Passenger p in Occupant)
+            {
+              wt=wt+ p.GetWeight();
+            }
+            return wt;
+         }
+
+        public bool IsOverMaxCapacity()
+        {
+            
+            if (GetCurrentWeight() > MaxWeight)
+            { return true; }
+            else { return false; }
+        }
+    }
+}
